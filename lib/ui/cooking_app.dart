bd0f92d51a3e585103297c40d62daa6d72b03636
yumayuma708 <yumaskywalker708@gmail.com>
2023-09-06 03:52:25 +0900

    docs: その他のディレクトリ・フォルダを整備する

---
+++ lib/ui/cooking_app.dart
@@ -0,0 +1,18 @@
+import 'package:caul/ui/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class CookingApp extends StatelessWidget {
+  const CookingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cooking As You Like',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
