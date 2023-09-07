a91135815d74e7d619331eb5639bc3cbb346d887
yumayuma708 <yumaskywalker708@gmail.com>
2023-09-07 19:25:56 +0900

    feat: マイページにメールアドレス入力欄と「次へ」ボタンを追加

--- lib/ui/screens/my_page_screen.dart
+++ lib/ui/screens/my_page_screen.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
 
-class MyPageScreen extends StatelessWidget {
+class MyPageScreen extends StatefulWidget {
   const MyPageScreen({Key? key}) : super(key: key);
+
+  @override
+  _MyPageScreenState createState() => _MyPageScreenState();
+}
+
+class _MyPageScreenState extends State<MyPageScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _showLoginPopup();
+    });
+  }
+
+  _showLoginPopup() {
+    showDialog(
+      context: context,
+      barrierDismissible: false, // ユーザーがダイアログの外部をタップしたときにダイアログが閉じないようにする
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Googleにログイン'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'メールアドレス',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20), // 余白を追加
+              ElevatedButton(
+                child: Text('次へ'),
+                onPressed: () {
+                  Navigator.of(context).pop(); // ポップアップを閉じる
+                },
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
