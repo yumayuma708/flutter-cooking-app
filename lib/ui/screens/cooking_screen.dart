aa1bec87d77a4c89e165f94e477124347705d37a
yumayuma708 <yumaskywalker708@gmail.com>
2023-09-08 22:54:03 +0900

    feat: 料理ページを開いたら、ポップアップが表示されるように設定,ポップアップを数ページ作成 、ポップアップたちは横にスクロール可能にする

--- lib/ui/screens/cooking_screen.dart
+++ lib/ui/screens/cooking_screen.dart
@@ -1,15 +1,90 @@
 import 'package:flutter/material.dart';
 
-class CookingScreen extends StatelessWidget {
+class CookingScreen extends StatefulWidget {
   const CookingScreen({Key? key}) : super(key: key);
+
+  @override
+  _CookingScreenState createState() => _CookingScreenState();
+}
+
+class _CookingScreenState extends State<CookingScreen> {
+  late PageController _pageController;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      _showPopup(context);
+    });
+  }
+
+  _showPopup(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Dialog(
+          child: SizedBox(
+            height: 300,
+            child: Column(
+              children: [
+                Expanded(
+                  child: PageView(
+                    controller: _pageController,
+                    onPageChanged: (int page) {
+                      setState(() {
+                        _currentPage = page;
+                      });
+                    },
+                    children: const [
+                      Center(child: Text('ページ 1')),
+                      Center(child: Text('ページ 2')),
+                      Center(child: Text('ページ 3')),
+                    ],
+                  ),
+                ),
+                DotsIndicator(currentPage: _currentPage),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.orange[400], // 背景色をオレンジに設定
+        color: Colors.orange[400],
         child: const Center(child: Text('料理')),
       ),
+    );
+  }
+}
+
+class DotsIndicator extends StatelessWidget {
+  final int currentPage;
+
+  const DotsIndicator({super.key, required this.currentPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(3, (index) {
+        return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 4.0),
+          width: 8.0,
+          height: 8.0,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: index == currentPage ? Colors.blue : Colors.grey,
+          ),
+        );
+      }),
     );
   }
 }
