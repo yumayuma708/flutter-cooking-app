f61bc737fca3c2a191cec499e2e22ae61766badb
yumayuma708 <yumaskywalker708@gmail.com>
2023-09-12 14:34:58 +0900

    feat: 食材を選択する機能を実装

--- lib/ui/screens/cooking_screen.dart
+++ lib/ui/screens/cooking_screen.dart
@@ -12,9 +12,49 @@ class CookingScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Container(
-        color: Colors.orange[400],
+      appBar: AppBar(
+        title: const Text('食材を選ぶ'), // <-- 追加: タイトル
       ),
+      body: VegetablesListView(), // <-- 追加: リストビュー
+    );
+  }
+}
+
+class VegetablesListView extends StatefulWidget {
+  @override
+  _VegetablesListViewState createState() => _VegetablesListViewState();
+}
+
+class _VegetablesListViewState extends State<VegetablesListView> {
+  // 野菜のリストを作成
+  List<String> vegetables = List.generate(50, (index) => '野菜$index');
+  List<String> selectedVegetables = []; // <-- 追加: 選択された野菜を保存するリスト
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: vegetables.length,
+      itemBuilder: (context, index) {
+        return InkWell(
+          onTap: () {
+            setState(() {
+              if (selectedVegetables.contains(vegetables[index])) {
+                selectedVegetables.remove(vegetables[index]);
+              } else {
+                selectedVegetables.add(vegetables[index]);
+              }
+            });
+          },
+          child: Card(
+            color: selectedVegetables.contains(vegetables[index])
+                ? Colors.green[200]
+                : null,
+            child: ListTile(
+              title: Text(vegetables[index]),
+            ),
+          ),
+        );
+      },
     );
   }
 }
