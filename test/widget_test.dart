0f2180ea48c11e4b1684a77cd213ea396a8cb1de
yumayuma708 <yumaskywalker708@gmail.com>
2023-09-18 18:52:18 +0900

    fix: testファイルのバグ修正

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,12 +9,10 @@ import 'package:caul/ui/cooking_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:caul/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const CookingApp());
+    await tester.pumpWidget(CookingApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
