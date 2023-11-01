4c9638dc354a977c98797458836d5fa6228c0785
yumayuma708 <yumaskywalker708@gmail.com>
2023-11-01 21:36:34 +0900

    refactor: ボタンをコンポーネント化する

--- lib/ui/cooking_app.dart
+++ lib/ui/cooking_app.dart
@@ -1,5 +1,8 @@
 import 'package:caul/ui/screens/home_screen.dart';
+import 'package:firebase_ui_localizations/firebase_ui_localizations.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class CookingApp extends StatelessWidget {
   const CookingApp({super.key});
@@ -13,13 +16,26 @@ class CookingApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: seedColor,
         brightness: Brightness.light,
+        fontFamily: kIsWeb ? 'NotoSansJP' : null,
       ),
       darkTheme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: seedColor,
         brightness: Brightness.dark,
+        fontFamily: kIsWeb ? 'NotoSansJP' : null,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      locale: const Locale('ja', 'JP'),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        FirebaseUILocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ja', 'JP'),
+      ],
     );
   }
 }
