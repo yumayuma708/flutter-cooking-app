e86dbe60bc3b7c07e1854d4b7780a7bd6ee1a7e5
yumayuma708 <yumaskywalker708@gmail.com>
2023-11-02 18:11:43 +0900

    feat: ダークモードとライトモードを切り替えられるようにする。

--- lib/ui/cooking_app.dart
+++ lib/ui/cooking_app.dart
@@ -1,12 +1,16 @@
 import 'package:caul/ui/screens/home_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:caul/providers/theme_provider.dart'; // 必要に応じて適切なパスに変更してください
 
-class CookingApp extends StatelessWidget {
-  const CookingApp({super.key});
+class CookingApp extends ConsumerWidget {
+  const CookingApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(themeProvider); // ThemeProviderの状態を取得
+
     const seedColor = Colors.orange;
     return MaterialApp(
       title: 'Cooking As You Like',
@@ -22,6 +26,9 @@ class CookingApp extends StatelessWidget {
         brightness: Brightness.dark,
         fontFamily: kIsWeb ? 'ZenKakuGothicNew' : null,
       ),
+      themeMode: theme.isDarkMode
+          ? ThemeMode.dark
+          : ThemeMode.light, // ここでテーマモードを切り替える
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
       debugShowCheckedModeBanner: false,
     );
